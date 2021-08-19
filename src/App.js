52bdd14e44dc8775aaeee3794cbe9ee363f17df7
darwinbalino <darwinbalino@gmail.com>
2021-08-19 12:03:08 -0700

    pokemon fetch with zustand state manager

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import create from "zustand";
+import "./App.css";
+
+const POKEMON_URL =
+  "https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823/pokemon.json";
+
+export const useStore = create((set) => ({
+  filter: "",
+  pokemon: [],
+  setFilter: (filter) =>
+    set((state) => ({
+      ...state,
+      filter,
+    })),
+  setPokemon: (pokemon) =>
+    set((state) => ({
+      ...state,
+      pokemon,
+    })),
+}));
+
+const FilterInput = () => {
+  const filter = useStore((state) => state.filter);
+  const setFilter = useStore((state) => state.setFilter);
+  return <input value={filter} onChange={(e) => setFilter(e.target.value)} />;
+};
+
+const PokemonTable = () => {
+  const pokemon = useStore((state) => state.pokemon);
+
+  const filter = useStore((state) => state.filter);
+
+  return (
+    <table width="100%">
+      <tbody>
+        {pokemon
+          .filter(({ name: { english } }) =>
+            english.toLowerCase().includes(filter.toLowerCase())
+          )
+          .map(({ id, name: { english }, type }) => (
+            <tr key={id}>
+              <td>{english}</td>
+              <td>{type.join(", ")}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+};
+
+fetch(POKEMON_URL)
+  .then((resp) => resp.json())
+  .then((pokemon) =>
+    useStore.setState((state) => ({
+      ...state,
+      pokemon,
+    }))
+  );
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FilterInput />
+      <PokemonTable />
     </div>
   );
 }
